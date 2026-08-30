0d4a39f8d551ed21c23920af00d141ff784be1bd
Gilles Schippers <50411615+GustavoSchip@users.noreply.github.com>
2026-08-30 22:08:20 +0200

    Update Linter

--- .prettierrc.mjs
+++ .prettierrc.mjs
@@ -3,5 +3,46 @@ import { createRequire } from "node:module";
 const require = createRequire(import.meta.url);
 
 export default {
-    plugins: [require.resolve("@prettier/plugin-xml"), require.resolve("prettier-plugin-yaml")]
+    plugins: [require.resolve("@prettier/plugin-xml"), require.resolve("prettier-plugin-yaml")],
+    overrides: [
+        {
+            files: ["*.xml", "*.index"],
+            options: {
+                parser: "xml",
+                objectWrap: "collapse",
+                printWidth: 9999
+            }
+        },
+        {
+            files: ["*.yml"],
+            options: {
+                objectWrap: "collapse",
+                printWidth: 4
+            }
+        },
+        {
+            files: ["*.md"],
+            options: {
+                printWidth: 180
+            }
+        },
+        {
+            files: ["*.mjs"],
+            options: {
+                printWidth: 200
+            }
+        }
+    ],
+
+    printWidth: 160,
+    tabWidth: 4,
+    useTabs: false,
+    singleQuote: false,
+    requirePragma: false,
+    bracketSameLine: true,
+    bracketSpacing: true,
+    trailingComma: "none",
+    objectWrap: "preserve",
+    embeddedLanguageFormatting: "auto",
+    endOfLine: "lf"
 };
