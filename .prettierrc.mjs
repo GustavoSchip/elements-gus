534747b84e7053e4e160a7496ace04606783f1ce
Gustavo Schip <50411615+GustavoSchip@users.noreply.github.com>
2026-08-31 15:21:59 +0200

    Make experimental changes stable (#20)
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- .prettierrc.mjs
+++ .prettierrc.mjs
@@ -5,9 +5,10 @@ const require = createRequire(import.meta.url);
 export default {
     plugins: [require.resolve("@prettier/plugin-xml"), require.resolve("prettier-plugin-yaml")],
     overrides: [
-        { files: ["*.index"], options: { parser: "xml", xmlQuoteAttributes: "double", xmlWhitespaceSensitivity: "ignore", printWidth: 9999 } },
-        { files: ["*.xml"], options: { xmlQuoteAttributes: "double", xmlWhitespaceSensitivity: "ignore", printWidth: 9999 } },
+        { files: ["*.index"], options: { parser: "xml", xmlQuoteAttributes: "double", xmlSortAttributesByKey: true, xmlWhitespaceSensitivity: "ignore", printWidth: 9999 } },
+        { files: ["*.xml"], options: { xmlQuoteAttributes: "double", xmlSortAttributesByKey: true, xmlWhitespaceSensitivity: "ignore", printWidth: 9999 } },
         { files: ["*.yml"], options: { yamlBlockStyle: "folded", yamlCollectionStyle: "block", yamlQuoteValues: true, printWidth: 4 } },
+        { files: ["*.yaml"], options: { yamlBlockStyle: "folded", yamlCollectionStyle: "block", yamlQuoteValues: true, printWidth: 4 } },
         { files: ["*.md"], options: { printWidth: 180 } },
         { files: ["*.mjs"], options: { printWidth: 200 } }
     ],
