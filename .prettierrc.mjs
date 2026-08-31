442fa834af3e9958acdf06b9e55325359a48cd03
Gilles Schippers <50411615+GustavoSchip@users.noreply.github.com>
2026-08-31 13:54:11 +0200

    Reconfigure formatter

--- .prettierrc.mjs
+++ .prettierrc.mjs
@@ -8,6 +8,7 @@ export default {
         { files: ["*.index"], options: { parser: "xml", xmlQuoteAttributes: "double", xmlSortAttributesByKey: true, xmlWhitespaceSensitivity: "ignore", printWidth: 9999 } },
         { files: ["*.xml"], options: { xmlQuoteAttributes: "double", xmlSortAttributesByKey: true, xmlWhitespaceSensitivity: "ignore", printWidth: 9999 } },
         { files: ["*.yml"], options: { yamlBlockStyle: "folded", yamlCollectionStyle: "block", yamlQuoteValues: true, printWidth: 4 } },
+        { files: ["*.yaml"], options: { yamlBlockStyle: "folded", yamlCollectionStyle: "block", yamlQuoteValues: true, printWidth: 4 } },
         { files: ["*.md"], options: { printWidth: 180 } },
         { files: ["*.mjs"], options: { printWidth: 200 } }
     ],
